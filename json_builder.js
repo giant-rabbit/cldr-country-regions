f566490a0666fbb0da88e7f9574a2180efe6ede3
Peter Haight <peterh@giantrabbit.com>
2019-03-10 21:31:03 -0700

    Reworked to output an ordered array
    I want ordered arrays because most of the time we'll be iterating
    through it in alphabetical order to display the countries and regions.

--- json_builder.js
+++ json_builder.js
@@ -4,12 +4,16 @@ var path = require('path');
 var process = require('process');
 
 class Country {
-  constructor() {
+  constructor(code) {
+    this.code = code;
     this.regions = [];
   }
 }
 
 class Region {
+  constructor(code) {
+    this.code = code;
+  }
 }
 
 function iterateTreeLeaves(tree, types, callback) {
@@ -29,13 +33,30 @@ function ensureDir(path) {
   }
 }
 
+function nameComparison(l, r) {
+  if (l.name < r.name) {
+    return -1;
+  } else if (l.name == r.name) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
 
 class JsonBuilder {
   constructor() {
-    this.countriesByCode = {};
-    this.regionsByCode = {};
-    this.countriesInEnglish = {};
-    this.regionsInEnglish = {};
+    this.countriesByCode = new Map();
+    this.regionsByCode = new Map();
+    this.countriesInEnglish = new Map();
+    this.regionsInEnglish = new Map();
+    this.regionsToSkip = [
+      'usas',
+      'usgu',
+      'usmp',
+      'uspr',
+      'usum',
+      'usvi',
+    ];
   }
 
   build() {
@@ -47,29 +68,42 @@ class JsonBuilder {
   }
 
   buildCountries() {
-    const countriesByCode = {};
+    const countriesByCode = new Map();
     const continentCodes = ['002', '019', '142', '150', '009'];
     const groups = cldr.extractTerritoryContainmentGroups();
-    iterateTreeLeaves(groups, continentCodes, (leaf) => { countriesByCode[leaf] = new Country(); });
+    iterateTreeLeaves(groups, continentCodes, (leaf) => { countriesByCode.set(leaf, new Country(leaf)); });
     return countriesByCode;
   }
 
+  buildOutputArray(countriesByCode, regionsByCode) {
+    const outputArray = [];
+    for (const [countryCode, country] of countriesByCode) {
+      outputArray.push(country);
+      country.regions.sort(nameComparison);
+    }
+    outputArray.sort(nameComparison);
+    return outputArray;
+  }
+
   buildRegions(countriesByCode) {
-    const regionsByCode = {};
+    const regionsByCode = new Map();
     const subdivisionPath = path.resolve(cldr.cldrPath, 'common', 'supplemental', 'subdivisions.xml');
     const subdivisionContainmentDoc = cldr.getDocument(subdivisionPath);
     const finder = cldr.createFinder([subdivisionContainmentDoc]);
     const subGroups = finder('/supplementalData/subdivisionContainment/subgroup');
     for (const subGroup of subGroups) {
       const countryCode = subGroup.getAttribute('type');
-      if (countryCode in countriesByCode) {
-        const country = countriesByCode[countryCode];
+      if (countriesByCode.has(countryCode)) {
+        const country = countriesByCode.get(countryCode);
         const contains = subGroup.getAttribute('contains');
         const regionCodes = contains.split(' ');
         for (const regionCode of regionCodes) {
+          if (this.regionsToSkip.indexOf(regionCode) != -1) {
+            continue;
+          }
           const region = new Region(regionCode);
-          regionsByCode[regionCode] = region;
-          country.regions.push(regionCode);
+          regionsByCode.set(regionCode, region);
+          country.regions.push(region);
         }
       }
     }
@@ -91,16 +125,16 @@ class JsonBuilder {
   }
 
   resetNames(objectsByCode, objectsInEnglish) {
-    for (const code in objectsByCode) {
-      objectsByCode[code].name = objectsInEnglish[code].name;
+    for (const [code, object] of objectsByCode) {
+      object.name = objectsInEnglish.get(code).name;
     }
   }
 
   updateCountryDataToLocale(countriesByCode, localeId) { 
     const territoryDisplayNames = cldr.extractTerritoryDisplayNames(localeId);
     for (const countryCode in territoryDisplayNames) {
-      if (countryCode in countriesByCode) {
-        const country = countriesByCode[countryCode];
+      if (countriesByCode.has(countryCode)) {
+        const country = countriesByCode.get(countryCode);
         const displayName = territoryDisplayNames[countryCode];
         country.name = displayName;
       }
@@ -116,8 +150,8 @@ class JsonBuilder {
     const subdivisions = finder('/ldml/localeDisplayNames/subdivisions/subdivision');
     for (const subdivision of subdivisions) {
       const regionCode = subdivision.getAttribute('type');
-      if (regionCode in regionsByCode) {
-        const region = regionsByCode[regionCode];
+      if (regionsByCode.has(regionCode)) {
+        const region = regionsByCode.get(regionCode);
         region.name = subdivision.textContent;
       }
     }
@@ -138,12 +172,11 @@ class JsonBuilder {
       this.resetNames(regionsByCode, this.regionsInEnglish);
       this.updateCountryDataToLocale(countriesByCode, localeId);
       this.updateRegionDataToLocale(regionsByCode, localeId);
+      const outputArray = this.buildOutputArray(countriesByCode, regionsByCode);
       const localePath = path.join(dataPath, localeId);
       ensureDir(localePath);
       const countriesPath = path.join(localePath, 'countries.json');
-      fs.writeFileSync(countriesPath, JSON.stringify(countriesByCode));
-      const regionsPath = path.join(localePath, 'regions.json');
-      fs.writeFileSync(regionsPath, JSON.stringify(regionsByCode));
+      fs.writeFileSync(countriesPath, JSON.stringify(outputArray));
       process.stdout.write("\n");
     }
   }
